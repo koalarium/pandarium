733be785bf75f493f327ced25303d4a69d2b17f4
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-02-12 18:49:59 +0100

    [UPDATE] tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [],
   images: {
