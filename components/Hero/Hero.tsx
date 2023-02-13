dbe029a27dc4743841f73623ff18e160c1237126
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-02-13 16:09:27 +0100

    [UPDATE] Blur on circles on Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -23,10 +23,10 @@ const Hero = () => {
                 <Button variant="0" content="zobacz gry"/>
                 </div>
 
-                <div className="w-[150px] md:w-[350px] aspect-square absolute animate-moveLeft -top-20 -left-10 bg-purple-300 rounded-full blur-[150px]"></div>
-                <div className="w-[250px] md:w-[450px] aspect-square absolute animate-moveLeft -top-20 right-96 bg-purple-300 rounded-full blur-[150px]"></div>
-                <div className="w-[500px] md:w-[700px] aspect-square absolute animate-moveRight -bottom-80 left-20 bg-purple-300 rounded-full blur-[150px]"></div>
-                <div className="w-[300px] md:w-[500px] aspect-square absolute animate-moveRight -bottom-40 -right-2 bg-purple-300 rounded-full blur-[150px]"></div>
+                <div className="w-[150px] md:w-[450px] delay-200 aspect-square absolute animate-moveLeft -top-20 -left-10 bg-purple-300 rounded-full blur-[150px]"></div>
+                <div className="w-[250px] md:w-[550px] delay-50 aspect-square absolute animate-moveLeft -top-20 right-96 bg-purple-300 rounded-full blur-[150px]"></div>
+                <div className="w-[500px] md:w-[800px] delay-0 aspect-square absolute animate-moveRight -bottom-80 left-20 bg-purple-300 rounded-full blur-[150px]"></div>
+                <div className="w-[300px] md:w-[600px] delay-300 aspect-square absolute animate-moveRight -bottom-40 -right-2 bg-purple-300 rounded-full blur-[150px]"></div>
 
             {/* <div className="w-40 h-40 bg-black"></div> */}
             </div>
