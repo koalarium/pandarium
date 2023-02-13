44b0268b34e84950276ead886d0c057a1090eee8
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-02-13 15:57:16 +0100

    [BUGFIX] Decresing spaces between clasess

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,15 +5,15 @@ const Hero = () => {
             <div className="text-center w-full flex flex-col md:flex-row top-0 h-4/5 justify-center align-center relative">
                 <div className="z-10">
                     <h1 className="text-2xl font-rubikbold mx-auto md:text-3xl md:hidden">Pandarium</h1>
-                    <p className="mt-2   font-rubik text-sm px-2 opacity-60 mx-auto md:hidden">Minigierki od graczy stworzone dla graczy</p>
-                    <button className=" relative uppercase font-rubikbold bg-purple-300 text-xs  py-2 px-10 rounded-xl mt-8 mx-auto z-50 md:hidden  ">Zobacz gry</button>
+                    <p className="mt-2 font-rubik text-sm px-2 opacity-60 mx-auto md:hidden">Minigierki od graczy stworzone dla graczy</p>
+                    <button className="relative uppercase font-rubikbold bg-purple-300 text-xs py-2 px-10 rounded-xl mt-8 mx-auto z-50 md:hidden  ">Zobacz gry</button>
                 </div>
 
                 {/* >=md: */}
                 <div className="hidden md:flex flex-col w-56 lg:w-64 justify-center align-left my-auto text-left z-10">
                     <h1 className="text-3xl font-rubikbold lg:text-4xl ">Pandarium</h1>
                     <p className="mt-2 font-rubik text-md opacity-60 lg:text-lg ">Minigierki od graczy stworzone dla graczy</p>
-                    <button className=" relative uppercase font-rubikbold bg-purple-300 text-sm  py-2 px-10 rounded-xl mt-8 mr-auto z-50 lg:text-md lg:py-3 lg:px-12">Zobacz gry</button>
+                    <button className=" relative uppercase font-rubikbold bg-purple-300 text-sm py-2 px-10 rounded-xl mt-8 mr-auto z-50 lg:text-md lg:py-3 lg:px-12">Zobacz gry</button>
                 </div>
                 <div className="hidden md:flex flex-col w-1/2 h-60 justify-center align-center md:my-auto">
                     
