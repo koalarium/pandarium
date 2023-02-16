080c9428907084e3de51686dcb18933c9a0ae941
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-02-16 13:56:41 +0100

    [BUGFIX] Button fonts, sizes and margin which shoudn't be in default component set

--- components/Global/Button.tsx
+++ components/Global/Button.tsx
@@ -20,7 +20,7 @@ const Button = ({className, content,variant}: Props) => {
     }
 
     return (
-        <button className={`bg-purple-300 text-white text-xs sm:text-sm md:text-base mt-8 z-50 lg:text-md xl:text-lg lg:py-3 lg:px-12 uppercase font-bold px-10 py-2 rounded-lg hover:bg-[#c153e0] transition-all duration-300 ${btnBoxShadow}  ${btnActiveShadow} ${btnActiveTransform} ${className}`}>{content}</button>
+        <button className={`bg-purple-300 text-white text-md md:text-md z-50 lg:text-xl lg:py-4 lg:px-20 uppercase font-rubikbold px-12 py-4 rounded-2xl hover:bg-[#c153e0] transition-all duration-300 ${btnBoxShadow}  ${btnActiveShadow} ${btnActiveTransform} ${className}`}>{content}</button>
     );
 }
 
