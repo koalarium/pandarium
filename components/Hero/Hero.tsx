718bbb4c8b85c758833a9cad976c4e8234129aad
Krawieckk <krawieck03@gmail.com>
2023-02-16 13:46:04 +0100

    [UPDATE] Made fonts bigger in Hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
                 <div className="text-center w-screen flex flex-col md:flex-row top-0 h-4/5 justify-center items-center relative">
                     <div className="z-10 w-full flex flex-col justify-center items-center md:text-left md:w-1/2">
                         <div>
-                            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-rubikbold">Pandarium</h1>
-                            <p className="mt-2 font-rubik text-sm sm:text-base lg:text-lg xl:text-xl px-2 opacity-60 md:px-0 max-w-[20ch] md:max-w-[18ch] xl:max-w-[20ch]">Minigierki od graczy stworzone dla graczy</p>
+                            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-rubikbold">Pandarium</h1>
+                            <p className="mt-2 font-rubik text-base sm:text-lg lg:text-xl xl:text-xl 2xl:text-2xl px-2 opacity-60 md:px-0 max-w-[20ch] md:max-w-[22ch]">Minigierki od graczy stworzone dla graczy</p>
                             <div className="">
                                 <Button className="md:mr-auto" variant="0" content="zobacz gry" />
                             </div>
@@ -16,7 +16,7 @@ const Hero = () => {
                     </div>
                     
                     <div className="hidden md:flex flex-col w-1/2 h-60 justify-center items-center md:my-auto">
-                        <img className="z-10 w-[220px] lg:w-[300px] -rotate-[20deg] -translate-y-5 animate-pandaHero" src="images\pandas\lion\lion-500px-outline.png" alt="Panda dressed in lion costume"/>
+                        <img className="z-10 w-[220px] lg:w-[300px] xl:w-[340px] -rotate-[20deg] -translate-y-5 animate-pandaHero" src="images\pandas\lion\lion-500px-outline.png" alt="Panda dressed in lion costume"/>
                     </div>
 
                     <div className="w-[150px] md:w-[450px] delay-200 aspect-square absolute animate-moveLeft -top-20 -left-10 bg-purple-300 rounded-full blur-[150px]"></div>
