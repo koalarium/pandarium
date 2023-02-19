8dbd14e88a44b4b8321d8c7ffc15f865ff35ac12
Krawieckk <krawieck03@gmail.com>
2023-02-19 21:59:09 +0100

    [BUGFIX] Fixed imports in test.tsx

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,7 +2,6 @@ import CTA from 'components/CTA/CTA'
 import { LgContainer, MdContainer, SmContainer } from 'components/Global/Containers'
 import GamesCarousel from 'components/Global/GameCarousel'
 import Title from 'components/Global/Title'
-import Hero from 'components/Hero/Hero'
 import Us from 'components/Us/Us'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -10,7 +9,7 @@ import Article from 'components/Article/Article'
 import Date from 'components/Article/Date'
 import Hashtag from 'components/Article/Hashtag'
 import ArticleTitle from 'components/Article/ArticleTitle'
-import HeroSmall from 'components/Hero/HeroSmall'
+import {Hero, HeroSmall, HeroLogin} from 'components/Hero/Heroes'
 
 export default function LandingPage() {
   return (	
