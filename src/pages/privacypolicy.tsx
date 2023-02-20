bd1bd1490e74fbd666cc49244a6d4371d510e246
ignatz <ignacy.zielinski@wp.pl>
2023-02-20 16:52:23 +0100

    [NEW] Privacy policy and cookies

---
+++ src/pages/privacypolicy.tsx
@@ -0,0 +1,28 @@
+import { XlContainer } from "components/Global/Containers";
+
+const PrivacyPolicy = () => {
+    return (
+        <XlContainer >
+            <div className="border-purple-900 border-2 rounded-2xl h-fit w-fit mx-auto">
+                <div className="flex flex-col justify px-12">
+                    <div className="w-2/3 h-[12px] bg-purple-500 rounded-md mx-auto mt-4 mb-1 opacity-60 md:mt-4"></div>
+                    <h1 className="text-center font-rubikbold text-purple-900 uppercase mb-2 mt-2 md:text-3xl md:mb-4">Polityka prywatności</h1>  
+                </div>
+                <div className="px-4 mb-1">
+                    <h2 className="font-rubikbold mb-1 ml-2 md:text-xl">Kim jesteśmy?</h2>
+                    <p className="leading-relaxed mx-2 text-sm my-2 indent-4 text-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam neque deleniti repellat suscipit minus atque, ab itaque architecto ea dolores ratione quam tenetur inventore! Ipsa, iste minus eaque esse eligendi, quibusdam nam aperiam eos dolore odio similique porro veritatis nihil. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ipsum id aspernatur illo quasi similique ratione distinctio, facere assumenda fuga eius sed mollitia vitae est?</p>
+                </div>
+                <div className="px-4 mb-1">
+                    <h3 className="font-rubikbold mb-1 ml-2 md:text-xl">Dlaczego zbieramy dane?</h3>
+                    <p className="leading-relaxed mx-2 text-sm my-2 indent-4 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quo tempora fuga ad dignissimos quae adipisci incidunt expedita dolore necessitatibus voluptatum illo maiores provident, voluptates quos fugit explicabo, ratione ullam delectus corporis quaerat quia aperiam recusandae hic. Expedita, commodi eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolores quis adipisci quod ea aliquam maiores voluptate rerum esse assumenda quae rem, officiis accusantium labore?</p>
+                </div>
+                <div className="px-4 mb-1">
+                    <h4 className="font-rubikbold mb-1 ml-2 md:text-xl">Uprawnienia Użytkownika</h4>
+                    <p className="leading-relaxed mx-2 text-sm my-2 indent-4 text-left pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint explicabo laboriosam veniam. Consequatur ipsa recusandae totam impedit perferendis fuga dolorum amet pariatur. Placeat veniam, nisi sunt magni minus dolorem eos reiciendis nobis quia, aperiam labore voluptatem corporis, sequi ipsa at! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, aliquid nesciunt aliquam sapiente rem ipsam libero harum quos molestias iure distinctio sunt tenetur. Ullam, dolore.</p>
+                </div>
+            </div>    
+        </XlContainer>              
+    );
+}
+
+export default PrivacyPolicy; 
