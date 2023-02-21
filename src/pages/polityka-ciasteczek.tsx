0731e0a8560e129fc567f95d322bf88dd5a77819
Krawieckk <krawieck03@gmail.com>
2023-02-21 18:02:07 +0100

    [UPDATE] Translated Cookies policy

--- src/pages/polityka-ciasteczek.tsx
+++ src/pages/polityka-ciasteczek.tsx
@@ -11,87 +11,87 @@ const Cookie = () => {
 
                     <h3 className="font-rubikbold text-2xl md:text-3xl xl:text-4xl text-purple-900 mb-10">Polityka ciasteczek</h3>
 
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">What Are Cookies</p>
-                    <p className="text-base md:text-lg mb-8">As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Czym są ciasteczka?</p>
+                    <p className="text-base md:text-lg mb-8">Zgodnie z powszechną praktyką w przypadku prawie wszystkich profesjonalnych witryn internetowych, ta witryna wykorzystuje pliki cookie, które są małymi plikami pobieranymi na Twój komputer, aby poprawić Twoje wrażenia. Na tej stronie opisano, jakie informacje zbierają, jak je wykorzystujemy i dlaczego czasami musimy przechowywać te pliki cookie. Udostępnimy również, w jaki sposób można zapobiec przechowywaniu tych plików cookie, jednak może to obniżyć lub „zepsuć” niektóre elementy funkcjonalności witryn.</p>
                     
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">How We Use Cookies</p>
-                    <p className="text-base md:text-lg mb-8">We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Jak używamy ciasteczek?</p>
+                    <p className="text-base md:text-lg mb-8">Używamy plików cookie z różnych powodów wyszczególnionych poniżej. Niestety w większości przypadków nie ma standardowych branżowych opcji wyłączania plików cookie bez całkowitego wyłączania funkcjonalności i funkcji dodawanych do tej witryny. Zaleca się pozostawienie wszystkich plików cookie, jeśli nie jesteś pewien, czy ich potrzebujesz, czy nie w przypadku, gdy są one wykorzystywane do świadczenia usługi, z której korzystasz.</p>
                    
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Disabling Cookies</p>
-                    <p className="text-base md:text-lg mb-8">You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies. This Cookies Policy was created with the help of the <a href="https://www.cookiepolicygenerator.com/cookie-policy-generator/">Cookies Policy Generator</a>.</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Wyłączenie ciasteczek</p>
+                    <p className="text-base md:text-lg mb-8">Możesz zapobiec ustawianiu plików cookie, dostosowując ustawienia w swojej przeglądarce (zobacz Pomoc przeglądarki, aby dowiedzieć się, jak to zrobić). Pamiętaj, że wyłączenie plików cookie wpłynie na funkcjonalność tej i wielu innych witryn, które odwiedzasz. Wyłączenie plików cookie zwykle skutkuje również wyłączeniem niektórych funkcji i funkcji tej witryny. Dlatego zaleca się, aby nie wyłączać plików cookie. Niniejsza Polityka plików cookie została stworzona przy pomocy firmy <a className="text-purple-300" href="https://www.cookiepolicygenerator.com/cookie-policy-generator/">Cookies Policy Generator</a>.</p>
                     
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">The Cookies We Set</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Ciasteczka, z których korzystamy</p>
 
                     <ul>
 
                         <li>
-                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Account related cookies</p>
-                            <p className="text-base md:text-lg mb-8">If you create an account with us then we will use cookies for the management of the signup process and general administration. These cookies will usually be deleted when you log out however in some cases they may remain afterwards to remember your site preferences when logged out.</p>
+                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Ciasteczka związane z kontem</p>
+                            <p className="text-base md:text-lg mb-8">Jeśli utworzysz u nas konto, będziemy używać plików cookie do zarządzania procesem rejestracji i ogólnej administracji. Te pliki cookie są zwykle usuwane po wylogowaniu, jednak w niektórych przypadkach mogą pozostać później, aby zapamiętać preferencje witryny po wylogowaniu.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Login related cookies</p>
-                            <p className="text-base md:text-lg mb-8">We use cookies when you are logged in so that we can remember this fact. This prevents you from having to log in every single time you visit a new page. These cookies are typically removed or cleared when you log out to ensure that you can only access restricted features and areas when logged in.</p>
+                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Ciasteczka związane z logowaniem</p>
+                            <p className="text-base md:text-lg mb-8">Używamy ciasteczek, gdy jesteś zalogowany, abyśmy pamiętali o tym fakcie. Zapobiega to konieczności logowania się za każdym razem, gdy odwiedzasz nową stronę. Te pliki cookie są zazwyczaj usuwane lub czyszczone po wylogowaniu, aby zapewnić dostęp do zastrzeżonych funkcji i obszarów tylko po zalogowaniu.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Surveys related cookies</p>
-                            <p className="text-base md:text-lg mb-8">From time to time we offer user surveys and questionnaires to provide you with interesting insights, helpful tools, or to understand our user base more accurately. These surveys may use cookies to remember who has already taken part in a survey or to provide you with accurate results after you change pages.</p>
+                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Ciasteczka związane z ankietami</p>
+                            <p className="text-base md:text-lg mb-8">Od czasu do czasu oferujemy ankiety i kwestionariusze dla użytkowników, aby zapewnić interesujące spostrzeżenia, pomocne narzędzia lub dokładniej zrozumieć naszą bazę użytkowników. Ankiety te mogą wykorzystywać pliki cookie do zapamiętywania, kto już brał udział w ankiecie lub do dostarczania dokładnych wyników po zmianie stron.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Forms related cookies</p>
-                            <p className="text-base md:text-lg mb-8">When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
+                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Ciasteczka związane z formularzami</p>
+                            <p className="text-base md:text-lg mb-8">Gdy przesyłasz dane za pośrednictwem formularza, takiego jak te znajdujące się na stronach kontaktowych lub formularzach komentarzy, pliki cookie mogą zapamiętywać dane użytkownika na potrzeby przyszłej korespondencji.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Site preferences cookies</p>
-                            <p className="text-base md:text-lg mb-8">In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
+                            <p className="text-base md:text-lg mb-2 font-bold text-purple-500">Ciasteczka związane z preferencjami witryny</p>
+                            <p className="text-base md:text-lg mb-8">Aby zapewnić Ci doskonałe wrażenia z korzystania z tej witryny, udostępniamy funkcję ustawiania preferencji dotyczących sposobu działania tej witryny podczas korzystania z niej. Aby zapamiętać Twoje preferencje, musimy ustawić pliki cookie, aby te informacje mogły być wywoływane za każdym razem, gdy Twoje preferencje wpływają na interakcję ze stroną.</p>
                         </li>
 
                     </ul>
 
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Third Party Cookies</p>
-                    <p className="text-base md:text-lg mb-8">In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Pliki cookie stron trzecich</p>
+                    <p className="text-base md:text-lg mb-8">W niektórych szczególnych przypadkach używamy również plików cookie dostarczanych przez zaufane strony trzecie. Poniższa sekcja zawiera szczegółowe informacje na temat plików cookie stron trzecich, które możesz napotkać za pośrednictwem tej witryny.</p>
 
                     <ul>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</p>
-                            <p className="text-base md:text-lg mb-8">For more information on Google Analytics cookies, see the official Google Analytics page.</p>
+                            <p className="text-base md:text-lg mb-8">Ta strona korzysta z Google Analytics, które jest jednym z najbardziej rozpowszechnionych i zaufanych rozwiązań analitycznych w sieci, które pomaga nam zrozumieć, w jaki sposób korzystasz z witryny i jak możemy poprawić Twoje wrażenia. Te pliki cookie mogą śledzić takie rzeczy, jak czas spędzony w witrynie i odwiedzane strony, dzięki czemu możemy nadal tworzyć interesujące treści.</p>
+                            <p className="text-base md:text-lg mb-8">Więcej informacji na temat plików cookie Google Analytics można znaleźć na oficjalnej stronie Google Analytics.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.</p>
+                            <p className="text-base md:text-lg mb-8">Od czasu do czasu testujemy nowe funkcje i wprowadzamy subtelne zmiany w sposobie dostarczania witryny. Kiedy wciąż testujemy nowe funkcje, te pliki cookie mogą być używane w celu zapewnienia spójnego korzystania z witryny, a jednocześnie upewnienia się, że rozumiemy, które optymalizacje nasi użytkownicy cenią sobie najbardziej.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
+                            <p className="text-base md:text-lg mb-8">Ponieważ sprzedajemy produkty, ważne jest dla nas zrozumienie statystyk dotyczących liczby osób odwiedzających naszą witrynę, które faktycznie dokonują zakupu, i jako takie są to dane, które będą śledzone przez te pliki cookie. Jest to dla Ciebie ważne, ponieważ oznacza, że ​​możemy dokładnie prognozować biznes, co pozwala nam monitorować koszty naszych reklam i produktów, aby zapewnić najlepszą możliwą cenę.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">The Google AdSense service we use to serve advertising uses a DoubleClick cookie to serve more relevant ads across the web and limit the number of times that a given ad is shown to you.</p>
-                            <p className="text-base md:text-lg mb-8">For more information on Google AdSense see the official Google AdSense privacy FAQ.</p>
+                            <p className="text-base md:text-lg mb-8">Usługa Google AdSense, której używamy do wyświetlania reklam, wykorzystuje plik cookie DoubleClick, aby wyświetlać trafniejsze reklamy w Internecie i ograniczać liczbę wyświetleń danej reklamy.</p>
+                            <p className="text-base md:text-lg mb-8">Aby uzyskać więcej informacji na temat Google AdSense, zobacz oficjalne często zadawane pytania dotyczące prywatności Google AdSense.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">We use adverts to offset the costs of running this site and provide funding for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant adverts where possible by anonymously tracking your interests and presenting similar things that may be of interest.</p>
+                            <p className="text-base md:text-lg mb-8">Używamy reklam, aby zrekompensować koszty prowadzenia tej witryny i zapewnić fundusze na dalszy rozwój. Pliki cookie reklamy behawioralnej używane przez tę witrynę mają na celu zapewnienie, że w miarę możliwości dostarczamy Ci najbardziej odpowiednie reklamy, anonimowo śledząc Twoje zainteresowania i prezentując podobne rzeczy, które mogą Cię zainteresować.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">Several partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and where applicable allow our affiliate partners to provide any bonus that they may provide you for making a purchase.</p>
+                            <p className="text-base md:text-lg mb-8">Kilku partnerów reklamuje się w naszym imieniu, a śledzące pliki cookie podmiotów stowarzyszonych pozwalają nam po prostu sprawdzić, czy nasi klienci odwiedzili witrynę za pośrednictwem jednej z naszych witryn partnerskich, abyśmy mogli odpowiednio je zaksięgować i, w stosownych przypadkach, umożliwić naszym partnerom stowarzyszonym zapewnienie wszelkich premii, jakie mogą zapewnić Państwu dokonanie zakupu.</p>
                         </li>
 
                         <li>
-                            <p className="text-base md:text-lg mb-8">We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including Facebook, Google, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+                            <p className="text-base md:text-lg mb-8">Na tej stronie używamy również przycisków i/lub wtyczek mediów społecznościowych, które umożliwiają łączenie się z siecią społecznościową na różne sposoby. Aby działały, następujące serwisy społecznościowe, w tym Facebook, Google, ustawią pliki cookie za pośrednictwem naszej witryny, które mogą zostać wykorzystane do ulepszenia Twojego profilu na ich stronie lub wniesienia wkładu w dane, które przechowują, do różnych celów określonych w ich odpowiednich politykach prywatności.</p>
                         </li>
 
                     </ul>
 
-                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">More Information</p>
-                    <p className="text-base md:text-lg mb-8">Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site.</p>
-                    <p className="text-base md:text-lg mb-8">For more general information on cookies, please read <a href="https://www.cookiepolicygenerator.com/sample-cookies-policy/">the Cookies Policy article</a>.</p>
-                    <p className="text-base md:text-lg mb-8">However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+                    <p className="text-xl md:text-2xl  font-rubikbold text-purple-500 mb-4">Więcej informacji</p>
+                    <p className="text-base md:text-lg mb-8">Mamy nadzieję, że to wyjaśniło Ci sprawę i jak wspomniano wcześniej, jeśli jest coś, czego nie jesteś pewien, czy potrzebujesz, czy nie, zwykle bezpieczniej jest pozostawić włączoną obsługę plików cookie na wypadek, gdyby wchodziła w interakcję z jedną z funkcji używanych w naszej witrynie.</p>
+                    <p className="text-base md:text-lg mb-8">Aby uzyskać więcej ogólnych informacji na temat plików cookie, przeczytaj <a className="text-purple-300" href="https://www.cookiepolicygenerator.com/sample-cookies-policy/">artykuł o polityce plików Cookie</a>.</p>
+                    <p className="text-base md:text-lg mb-8">Jeśli jednak nadal szukasz więcej informacji, możesz skontaktować się z nami za pomocą jednej z naszych preferowanych metod kontaktu:</p>
 
                     <ul>
                         <li>Email: mateuszkrysiaczek@gmail.com</li>
