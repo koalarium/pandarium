2b69c85993aaadf7965b8ce9a4f6e6b1aff8f1c9
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-02-22 17:37:05 +0100

    [BUGFIX] Containers vertical mode

--- src/components/Global/Containers.tsx
+++ src/components/Global/Containers.tsx
@@ -15,14 +15,14 @@ export const SmContainer = ({ className, children, vertical=false, horizontal=tr
 
 export const MdContainer = ({ className, children, vertical=false, horizontal=true }: Props) => {
     return (
-        <div className={`${ vertical ? "my-12 md:my-16 lg:my-20 xl:my-24" : "" } ${ horizontal ? "2xl:my-28 mx-4 sm:mx-6 md:mx-14 lg:mx-16 xl:mx-20 2xl:mx-24" : "" } ${ className }`}>
+        <div className={`${ vertical ? "my-12 md:my-16 lg:my-20 xl:my-24 2xl:my-28" : "" } ${ horizontal ? " mx-4 sm:mx-6 md:mx-14 lg:mx-16 xl:mx-20 2xl:mx-24" : "" } ${ className }`}>
             { children }
         </div>
     );
 }
 export const LgContainer = ({ className, children, vertical=false, horizontal=true }: Props) => {
     return (
-        <div className={`${ vertical ? "my-12 md:my-16 lg:my-20 xl:my-24 2xl:my-28 mx-6" : "" } ${ horizontal ? "sm:mx-10 md:mx-16 lg:mx-24 xl:mx-28 2xl:mx-56" : "" } ${ className }`}>
+        <div className={`${ vertical ? "my-12 md:my-16 lg:my-20 xl:my-24 2xl:my-28" : "" } ${ horizontal ? "mx-6 sm:mx-10 md:mx-16 lg:mx-24 xl:mx-28 2xl:mx-56" : "" } ${ className }`}>
             { children }
         </div>
     );
@@ -36,9 +36,9 @@ export const XlContainer = ({ className, children, vertical=false, horizontal=tr
     );
 }
 
-export const TitleContainer = ({ className, children }: Props) => {
+export const TitleContainer = ({ className, children, vertical, horizontal=true }: Props) => {
     return (
-        <div className={`my-12 md:my-16 lg:my-20 xl:my-24 2xl:my-28 ${ className }`}>
+        <div className={`mb-12 md:mb-16 lg:mb-20 xl:mb-24 2xl:mb-28 ${ className }`}>
             { children }
         </div>
     );
