ff8303647b4ed3eb98fcca25e1de370ac3beac00
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-03-11 02:01:00 +0100

    [NEW] Login engine

---
+++ src/pages/api/user/register.js
@@ -0,0 +1,72 @@
+import clientPromise from "../../../../lib/dbConnect";
+import { hash } from "bcryptjs";
+
+export default async (req, res) => {
+
+    try {
+
+        if (req.method == "POST") {
+
+            const client = await clientPromise;
+            const db = client.db("pandarium");
+            
+            const { login, email, password, repeatPassword, birthday } = req.body;
+
+            if ( !login || !email || !password || !repeatPassword || !birthday || !email.includes('@') || !email.includes('.') ){
+                res.status(422).json({ message: "Niepełne lub błędne dane!" });
+                return;
+            }
+
+            const exists = await db.collection("users").findOne({ email: email });
+
+            if (exists) {
+                res.status(422).json({ message: "Podany email jest już w użyciu!"});
+                return;
+            }
+
+            if (password != repeatPassword){
+                res.status(422).json({ message: "Hasła nie zgadzają się!" });
+                return;
+            }
+
+            if (password.length < 8) {
+                res.status(422).json({ message: "Hasło zawiera mniej niż 8 znaków." });
+                return;
+            }
+
+            const today = new Date();
+            const brhd = birthday.split("-");
+            const birthdayTimestamp = new Date( brhd[2], brhd[1]-1, brhd[0] );
+             
+            if (today - birthdayTimestamp < 504921600) {
+                res.status(422).json({ message: "Osoby poniżej 16 roku życia nie moga korzystać z tej strony."})
+                return;
+            }
+
+            const user = {
+                nick: login,
+                email: email,
+                password: hash(password, 12),
+                birthday: birthday,
+                panda: "panda",
+                playedGames: 0,
+                wonGames: 0,
+            }
+
+            const status = await db.collection("users").insertOne({ user });
+            await res.status(201).json({ message: "Użytkownik został stworzony!"});
+            console.log(user);
+
+        } else {
+
+            res.status(422).json({ message: "Nie poprawne zapytanie!"});
+
+        }
+        
+    } catch (e) {
+
+        console.error(e);
+
+    }
+
+}
--- src/pages/rejestracja.tsx
+++ src/pages/rejestracja.tsx
@@ -4,17 +4,36 @@ import { TitleContainer } from "../components/Global/Containers";
 import Title from "../components/Global/Title";
 import { Input } from "../components/Forms/Input";
 import Button from "../components/Forms/Button";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import CirclesLayout from "./layouts/CirclesLayout";
 
 const Rejestracja = () => {
 
         const [login, setLogin] = useState("");
+        const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
         const [repeatPassword, setRepeatPassword] = useState("");
-        const [age, setAge] = useState("");
+        const [birthday, setBirthday] = useState("");
         const [acceptTerms, setAcceptTerms] = useState(false);
+        const [result, setResult] = useState<any>(false);
+
+        const submitHandler = async () => {
+
+            const data = { login, email, password, repeatPassword, birthday }
+
+            const res = await fetch('/api/user/register', {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            });
+
+            setResult(await res.json());
+            console.log(result);
+
+        }
 
         useEffect(() => {
             
@@ -37,12 +56,16 @@ const Rejestracja = () => {
                         <h2 className="font-rubikbold uppercase text-xl sm:text-3xl md:text-4xl xl:text-5xl text-center">Rejestacja</h2>
                     </div>
                     <div className="w-[100%] xl:w-[80%] mx-auto flex flex-col gap-3">
-                        <Input onChange={ (e: any) => { setLogin(e.target.value) } } value={ login } placeholder="E-mail" type="email"/>
-                        <Input onChange={ (e: any) => { setAge(e.target.value) } } value={ age } placeholder="Wiek" type="number"/>
-                        <Input onChange={ (e: any) => { setPassword(e.target.value) } } value={ password } placeholder="Hasło" type="password"/>
-                        <Input onChange={ (e: any) => { setRepeatPassword(e.target.value) } } value={ repeatPassword } placeholder="Potwierdź hasło" type="password"/>
-                        <Input onChange={ (e: any) => { setAcceptTerms(e.target.value) } } value={ acceptTerms } label="[WARUNKI]" type="checkbox" className="mr-auto"/>
-                        <Button className="mx-auto mt-6 md:mt-12">Zarejestruj się</Button>
+                        { result && result.message ?
+                            <p className="text-white font-rubikbold text-2xl">{ result.message }</p> 
+                        : <></>}
+                        <Input onChange={ (e: any) => { setLogin(e.target.value) } } value={ login } placeholder="Nick" type="text" required/>
+                        <Input onChange={ (e: any) => { setEmail(e.target.value) } } value={ email } placeholder="E-mail" type="email" required/>
+                        <Input onChange={ (e: any) => { setBirthday(e.target.value) } } value={ birthday } placeholder="Data urodzenia" type="date" required/>
+                        <Input onChange={ (e: any) => { setPassword(e.target.value) } } value={ password } placeholder="Hasło" type="password" required/>
+                        <Input onChange={ (e: any) => { setRepeatPassword(e.target.value) } } value={ repeatPassword } placeholder="Potwierdź hasło" type="password" required/>
+                        <Input onChange={ (e: any) => { setAcceptTerms(e.target.value) } } value={ acceptTerms } label="[WARUNKI]" type="checkbox" className="mr-auto" required/>
+                        <Button onClick={ submitHandler } className="mx-auto mt-6 md:mt-12">Zarejestruj się</Button>
                     </div>
                 </div>
                 
