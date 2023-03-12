02cdb339ac10ede61927d511c5ee6bcfafb3c4eb
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2023-03-12 16:45:17 +0100

    [NEW] Error page

--- src/components/Forms/Button.tsx
+++ src/components/Forms/Button.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Button = ({ className, children, onClick, type="button" }: Props) => {
     return (
         <>
-            <button onClick={ onClick ? onClick : () => {} } type={ type } className={`${ className ? className : "" } hover:bg-white/[0.9] hover:text-purple-300 duration-300 bg-purple-300 rounded-xl py-3 px-8 md:px-24 w-fit font-rubikbold uppercase text-base xl:text-lg`}>{ children }</button> 
+            <button onClick={ onClick ? onClick : () => {} } type={ type } className={`${ className ? className : "" } hover:bg-white/[0.9] hover:text-purple-300 text-white duration-300 bg-purple-300 rounded-xl py-3 px-8 md:px-24 w-fit font-rubikbold uppercase text-base xl:text-lg`}>{ children }</button> 
         </>
     );
 }
